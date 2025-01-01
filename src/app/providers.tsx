3ba7c7ee0c267da2545dbfdc8d0e6a1fa58753c2
Yasin Karbasi <yasinkardev@gmail.com>
2025-01-01 23:23:03 +0330

    fixed never used errors

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,7 +5,7 @@ import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider, useTheme } from "next-themes";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const { theme, setTheme } = useTheme();
+  const { theme } = useTheme();
 
     return (
         <NextUIProvider className={theme}>
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Button } from '@nextui-org/react'
-import { User, Github } from 'lucide-react'
+import { User } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
 
