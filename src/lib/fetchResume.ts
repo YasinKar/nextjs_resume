b2cb0b465fe358dc0b828642b7675e7646f178f0
Yasin Karbasi <yasinkardev@gmail.com>
2025-01-09 23:24:02 +0330

    update env

--- src/lib/fetchResume.ts
+++ src/lib/fetchResume.ts
@@ -1,4 +1,4 @@
 export async function fetchResume() {
-    const response = await fetch(process.env.DOMAIN || 'http://localhost:3000/resume.json');
+    const response = await fetch(process.env.DOMAIN || 'https://yasin.monster/resume.json');
     return response.json();
 }
